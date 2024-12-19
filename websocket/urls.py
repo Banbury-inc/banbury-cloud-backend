93bd5073968f73afd4478f58b00b219d68d00059
Michael Mills <mmills6060@gmail.com>
2024-12-19 14:34:08 -0500

    more work on ws

--- websocket/urls.py
+++ websocket/urls.py
@@ -1,8 +1,7 @@
-
-from django.urls import path
-from .consumers import Download_File_Request, Live_Data  # Make sure this matches the actual name
+from django.urls import re_path
+from . import consumers
 
 websocket_urlpatterns = [
-    path('live_data/', Live_Data.as_asgi()),  # Make sure this matches the class in consumers.py
-    path('download_request/', Download_File_Request.as_asgi()),  # Make sure this matches the class in consumers.py
+    re_path(r'live_data/$', consumers.Live_Data.as_asgi()),
+    re_path(r'download_request/$', consumers.Download_File_Request.as_asgi()),
 ]
