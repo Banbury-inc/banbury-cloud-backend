a45e9eb8f205c9a100106f75831280d3e0c03d3a
Michael Mills <mmills6060@gmail.com>
2025-04-17 12:13:26 -0400

    fix test

--- websocket/urls.py
+++ websocket/urls.py
@@ -1,9 +1,6 @@
 from django.urls import re_path
-from . import old_consumers
 from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/consumer/(?P<device_id>[^/]+)/$', consumers.Consumer.as_asgi()),
-    re_path(r'ws/live_data/$', old_consumers.Live_Data.as_asgi()),
-    re_path(r'ws/download_request/$', old_consumers.Download_File_Request.as_asgi()),
 ]
