3911f7dce7bf48a070a5556d5d4dd7417eca5920
Michael Mills <mmills6060@gmail.com>
2026-01-05 15:49:59 -0500

    usage api endpoint

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path("reject_friend_request/", views.reject_friend_request, name="reject_friend_request"),
     path("get_user_friends/", views.get_user_friends, name="get_user_friends"),
     path("get_realtime_token/", views.get_realtime_token, name="get_realtime_token"),
+    path("usage_summary/", views.usage_summary, name="usage_summary"),
+    path("track_token_usage/", views.track_token_usage, name="track_token_usage"),
 ]
